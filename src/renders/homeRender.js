1b5213aa3f51db21cef57f281f27a99ad24f961b
Gblanquez <hellogabrielblanquez@gmail.com>
2023-12-13 09:57:51 +0000

    Commit V1.45

--- src/renders/homeRender.js
+++ src/renders/homeRender.js
@@ -17,132 +17,132 @@ initialLoad() {
     window.scrollTo(0, 0)
     document.body.style.overflow = 'hidden';
 
-    let animation = lottie.loadAnimation({
-        container: document.querySelector('.lottie_wrap'), // the dom element that will contain the animation
-        renderer: 'svg',
-        loop: false,
-        autoplay: false,
-        path: 'https://uploads-ssl.webflow.com/654ffd6810a99fe2eda01c9b/65735262e47f7cb0e69a0531_ShowreelLogo.json' // the path to the animation json
-      });
+//     let animation = lottie.loadAnimation({
+//         container: document.querySelector('.lottie_wrap'), // the dom element that will contain the animation
+//         renderer: 'svg',
+//         loop: false,
+//         autoplay: false,
+//         path: 'https://uploads-ssl.webflow.com/654ffd6810a99fe2eda01c9b/65735262e47f7cb0e69a0531_ShowreelLogo.json' // the path to the animation json
+//       });
       
-      // Define your custom ease
-      let customEase = CustomEase.create("custom", "M0,0,C0,0,0.13,0.34,0.238,0.442,0.305,0.506,0.322,0.514,0.396,0.54,0.478,0.568,0.468,0.56,0.522,0.584,0.572,0.606,0.61,0.719,0.714,0.826,0.798,0.912,1,1,1,1");
+//       // Define your custom ease
+//       let customEase = CustomEase.create("custom", "M0,0,C0,0,0.13,0.34,0.238,0.442,0.305,0.506,0.322,0.514,0.396,0.54,0.478,0.568,0.468,0.56,0.522,0.584,0.572,0.606,0.61,0.719,0.714,0.826,0.798,0.912,1,1,1,1");
       
-      // Get the elements
-      let load_nmb = document.querySelector('.load_nmb');
-      let load_h1 = document.querySelector('.load-h1');
-      let load_parent = document.querySelector('.load_parent');
-      let background_img_wrap = document.querySelector('.background_img_wrap');
-      let logoLink = document.querySelector('.navbar_child');
-      let navLines = document.querySelectorAll('[data-a="nav-line"]');
-      let navTxt = document.querySelectorAll('.link_textspan');
+//       // Get the elements
+//       let load_nmb = document.querySelector('.load_nmb');
+//       let load_h1 = document.querySelector('.load-h1');
+//       let load_parent = document.querySelector('.load_parent');
+//       let background_img_wrap = document.querySelector('.background_img_wrap');
+//       let logoLink = document.querySelector('.navbar_child');
+//       let navLines = document.querySelectorAll('[data-a="nav-line"]');
+//       let navTxt = document.querySelectorAll('.link_textspan');
       
-      // Create a timeline
-      let tl = gsap.timeline();
+//       // Create a timeline
+//       let tl = gsap.timeline();
       
-// Split the text into individual characters
-let load_h1_split = new SplitType(load_h1, { types: 'chars' });
+// // Split the text into individual characters
+// let load_h1_split = new SplitType(load_h1, { types: 'chars' });
 
-// Hide the characters initially
-gsap.set(load_h1_split.chars, { opacity: 0 });
-gsap.set(background_img_wrap, { opacity: 0 });
+// // Hide the characters initially
+// gsap.set(load_h1_split.chars, { opacity: 0 });
+// gsap.set(background_img_wrap, { opacity: 0 });
 
-// Start counting to 100% here
-let counter = { val: 0 };
+// // Start counting to 100% here
+// let counter = { val: 0 };
 
-// First animation
-gsap.to(counter, {
-  val: 100,
-  roundProps: "val",
-  ease: customEase, // Use your custom ease
-  onUpdate: function() {
-    // Update the text content
-    load_nmb.textContent = counter.val.toString();
-    animation.goToAndStop(counter.val, true);
-  },
-  duration: 3 // Adjust the duration as needed
-}).then(() => {
-  // Set the properties right before the second animation starts
-  gsap.set(load_h1_split.chars,{
-      y: '120%',
-      opacity: 0
-  })
+// // First animation
+// gsap.to(counter, {
+//   val: 100,
+//   roundProps: "val",
+//   ease: customEase, // Use your custom ease
+//   onUpdate: function() {
+//     // Update the text content
+//     load_nmb.textContent = counter.val.toString();
+//     animation.goToAndStop(counter.val, true);
+//   },
+//   duration: 3 // Adjust the duration as needed
+// }).then(() => {
+//   // Set the properties right before the second animation starts
+//   gsap.set(load_h1_split.chars,{
+//       y: '120%',
+//       opacity: 0
+//   })
 
-  gsap.to(load_nmb,{
-      y: '-120%',
-      duration: 1.4,
-      ease: 'expo.out',
-      opacity: 0
-  })
+//   gsap.to(load_nmb,{
+//       y: '-120%',
+//       duration: 1.4,
+//       ease: 'expo.out',
+//       opacity: 0
+//   })
 
-  // Second animation
-  gsap.fromTo(load_h1_split.chars, {
-    y: '120%',
-    opacity: 0
-  }, {
-    y: '0%',
-    opacity: 1, // Make the characters visible again
-    ease: 'expo.out',
-    stagger: 0.02, // Stagger the animation for each character
-    duration: 1.6 // Adjust the duration as needed
-  }).then(() => {
-// Third animation
-tl.to(load_h1_split.chars, {
-    y: '-120%',
-    opacity: 0,
-    ease: 'expo.out',
-    stagger: 0.01, // Stagger the animation for each character
-    duration: 1.4 // Adjust the duration as needed
-  }).set(load_parent, {
-    zIndex: -1
-  }).set(background_img_wrap, { // Set the initial state of the background
-    display: 'block',
-    y: '120%',
-    borderRadius: '8rem',
-    scale: 0.6,
-    opacity: 0,
-  }).to(background_img_wrap, {
-    y: '0%',
-    borderRadius: '0rem',
-    scale: 1,
-    ease: 'expo.out',
-    opacity: 1,
-    duration: 1.8 // Adjust the duration as needed
-  }, "+=0.1") // Start this animation 0.1 seconds after the previous one
-  .fromTo(navLines,{
-    width: '0%',
-    opacity: 0,
-    ease: 'expo.out',
-  },{
-    width: '100%',
-    ease: 'expo.out',
-    opacity: 1,
-    duration: 0.8,
-    stagger:{
-      each: 0.2
-    }
-  }, "+=0") // Start this animation 0.1 seconds after the previous one
-  .from(logoLink,{
-    y: '120%',
-    opacity: 0,
-    ease: 'expo.out',
-    duration: 0.8
-  }, "+=0") // Start this animation 0.1 seconds after the previous one
-  .from(navTxt,{
-    y: '120%',
-    opacity: 0,
-    ease: 'expo.out',
-    duration: 0.8,
-    stagger:{
-      each: 0.02
-    }
-  }, "+=0") // Start this animation 0.1 seconds after the previous one
-  .then(() =>{
-    document.body.style.overflow = 'auto';
-    load_parent.style.display = 'none';
-  });
-  });
-});
+//   // Second animation
+//   gsap.fromTo(load_h1_split.chars, {
+//     y: '120%',
+//     opacity: 0
+//   }, {
+//     y: '0%',
+//     opacity: 1, // Make the characters visible again
+//     ease: 'expo.out',
+//     stagger: 0.02, // Stagger the animation for each character
+//     duration: 1.6 // Adjust the duration as needed
+//   }).then(() => {
+// // Third animation
+// tl.to(load_h1_split.chars, {
+//     y: '-120%',
+//     opacity: 0,
+//     ease: 'expo.out',
+//     stagger: 0.01, // Stagger the animation for each character
+//     duration: 1.4 // Adjust the duration as needed
+//   }).set(load_parent, {
+//     zIndex: -1
+//   }).set(background_img_wrap, { // Set the initial state of the background
+//     display: 'block',
+//     y: '120%',
+//     borderRadius: '8rem',
+//     scale: 0.6,
+//     opacity: 0,
+//   }).to(background_img_wrap, {
+//     y: '0%',
+//     borderRadius: '0rem',
+//     scale: 1,
+//     ease: 'expo.out',
+//     opacity: 1,
+//     duration: 1.8 // Adjust the duration as needed
+//   }, "+=0.1") // Start this animation 0.1 seconds after the previous one
+//   .fromTo(navLines,{
+//     width: '0%',
+//     opacity: 0,
+//     ease: 'expo.out',
+//   },{
+//     width: '100%',
+//     ease: 'expo.out',
+//     opacity: 1,
+//     duration: 0.8,
+//     stagger:{
+//       each: 0.2
+//     }
+//   }, "+=0") // Start this animation 0.1 seconds after the previous one
+//   .from(logoLink,{
+//     y: '120%',
+//     opacity: 0,
+//     ease: 'expo.out',
+//     duration: 0.8
+//   }, "+=0") // Start this animation 0.1 seconds after the previous one
+//   .from(navTxt,{
+//     y: '120%',
+//     opacity: 0,
+//     ease: 'expo.out',
+//     duration: 0.8,
+//     stagger:{
+//       each: 0.02
+//     }
+//   }, "+=0") // Start this animation 0.1 seconds after the previous one
+//   .then(() =>{
+//     document.body.style.overflow = 'auto';
+//     load_parent.style.display = 'none';
+//   });
+//   });
+// });
 
 
 
