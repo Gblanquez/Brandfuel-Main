d5daa30d056e40314c88e4213b0c0dcd5c6aaa74
Gblanquez <hellogabrielblanquez@gmail.com>
2023-12-13 10:05:06 +0000

    Commit V.16

--- src/renders/homeRender.js
+++ src/renders/homeRender.js
@@ -14,8 +14,8 @@ initialLoad() {
         // run code that should only happen once for your site
 
     console.log('this is the initial load');
-    window.scrollTo(0, 0)
-    document.body.style.overflow = 'hidden';
+    // window.scrollTo(0, 0)
+    // document.body.style.overflow = 'hidden';
 
 //     let animation = lottie.loadAnimation({
 //         container: document.querySelector('.lottie_wrap'), // the dom element that will contain the animation
@@ -201,72 +201,72 @@ initialLoad() {
 
 
 
-  const cmsLink = document.querySelectorAll('[data-a="cms-link"]');
-    //  const cmsWrap = document.querySelectorAll('[data-a="cms-v-wrap"]');
-    //  const cmsVideo = document.querySelectorAll('[data-a="cms-v"]');
-    //  const cmsH2 = document.querySelectorAll('[data-a="cms-h2"]');
-    //  const cmsH3 = document.querySelectorAll('[data-a="cms-h3"]');
-    //  const cmsH4 = document.querySelectorAll('[data-a="cms-h4"]');
-    cmsLink.forEach((link) => {
-        const cmsWrap = link.querySelector('[data-a="cms-v-wrap"]');
-        const cmsVideo = link.querySelector('[data-a="cms-v"]');
-        const cmsH2 = link.querySelector('[data-a="cms-h2"]');
-        const cmsH3 = link.querySelector('[data-a="cms-h3"]');
-        const cmsH4 = link.querySelector('[data-a="cms-h4"]');
+  // const cmsLink = document.querySelectorAll('[data-a="cms-link"]');
+  //   //  const cmsWrap = document.querySelectorAll('[data-a="cms-v-wrap"]');
+  //   //  const cmsVideo = document.querySelectorAll('[data-a="cms-v"]');
+  //   //  const cmsH2 = document.querySelectorAll('[data-a="cms-h2"]');
+  //   //  const cmsH3 = document.querySelectorAll('[data-a="cms-h3"]');
+  //   //  const cmsH4 = document.querySelectorAll('[data-a="cms-h4"]');
+  //   cmsLink.forEach((link) => {
+  //       const cmsWrap = link.querySelector('[data-a="cms-v-wrap"]');
+  //       const cmsVideo = link.querySelector('[data-a="cms-v"]');
+  //       const cmsH2 = link.querySelector('[data-a="cms-h2"]');
+  //       const cmsH3 = link.querySelector('[data-a="cms-h3"]');
+  //       const cmsH4 = link.querySelector('[data-a="cms-h4"]');
     
-        // Split the text into characters
-        const cH2 = new SplitType(cmsH2, { types: 'chars' });
-        const cH3 = new SplitType(cmsH3, { types: 'chars' });
-        const cH4 = new SplitType(cmsH4, { types: 'chars' });
+  //       // Split the text into characters
+  //       const cH2 = new SplitType(cmsH2, { types: 'chars' });
+  //       const cH3 = new SplitType(cmsH3, { types: 'chars' });
+  //       const cH4 = new SplitType(cmsH4, { types: 'chars' });
     
-        const cmsTl = gsap.timeline({
-            scrollTrigger:{
-                trigger: link,
-                start: 'top bottom',
-                onEnter: () => cmsTl.restart(),
-                // onEnterBack: () => cmsTl.restart(),
-                // onLeave: () => cmsTl.restart(),
-            }
-        });
+  //       const cmsTl = gsap.timeline({
+  //           scrollTrigger:{
+  //               trigger: link,
+  //               start: 'top bottom',
+  //               onEnter: () => cmsTl.restart(),
+  //               // onEnterBack: () => cmsTl.restart(),
+  //               // onLeave: () => cmsTl.restart(),
+  //           }
+  //       });
     
-        cmsTl.from(cmsWrap, {
-            height: '0%',
-            duration: 1.6,
-            ease: 'expo.out',
-        }, 0.1);
+  //       cmsTl.from(cmsWrap, {
+  //           height: '0%',
+  //           duration: 1.6,
+  //           ease: 'expo.out',
+  //       }, 0.1);
     
-        cmsTl.from(cmsVideo, {
-            y: '-120%',
-            scale: 1.2,
-            ease: 'expo.out',
-            duration: 1.8
-        }, 0.1);
+  //       cmsTl.from(cmsVideo, {
+  //           y: '-120%',
+  //           scale: 1.2,
+  //           ease: 'expo.out',
+  //           duration: 1.8
+  //       }, 0.1);
     
-        // Animate the characters
-        cmsTl.from(cH2.chars, {
-            y: '120%',
-            opacity: 0,
-            stagger: 0.02,
-            ease: 'expo.out',
-            duration: 1.6
-        }, 0.1);
+  //       // Animate the characters
+  //       cmsTl.from(cH2.chars, {
+  //           y: '120%',
+  //           opacity: 0,
+  //           stagger: 0.02,
+  //           ease: 'expo.out',
+  //           duration: 1.6
+  //       }, 0.1);
     
-        cmsTl.from(cH3.chars, {
-            y: '120%',
-            opacity: 0,
-            stagger: 0.02,
-            ease: 'expo.out',
-            duration: 1.6
-        }, 0.1);
+  //       cmsTl.from(cH3.chars, {
+  //           y: '120%',
+  //           opacity: 0,
+  //           stagger: 0.02,
+  //           ease: 'expo.out',
+  //           duration: 1.6
+  //       }, 0.1);
     
-        cmsTl.from(cH4.chars, {
-            y: '120%',
-            opacity: 0,
-            stagger: 0.02,
-            ease: 'expo.out',
-            duration: 1.6
-        }, 0.1);
-    });
+  //       cmsTl.from(cH4.chars, {
+  //           y: '120%',
+  //           opacity: 0,
+  //           stagger: 0.02,
+  //           ease: 'expo.out',
+  //           duration: 1.6
+  //       }, 0.1);
+  //   });
     
 
       }
